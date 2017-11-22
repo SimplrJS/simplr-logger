00fe833b317f05047f591971af4b9aa99fb6d6f3
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-11-22 19:13:02 +0200

    Created contracts.

---
+++ @types/contracts/logger-configuration.d.ts
@@ -0,0 +1,11 @@
+import { LogLevelData, WriteMessageHandler, WriteMessageHandlerBuilder } from "simplr-logger";
+export interface LoggerConfigurationBase {
+    Prefix?: string;
+    DefaultLogLevel: LogLevelData;
+}
+export interface LoggerConfiguration extends LoggerConfigurationBase {
+    WriteMessageHandlers: WriteMessageHandler[];
+}
+export interface InitialLoggerConfiguration extends LoggerConfigurationBase {
+    WriteMessageHandlers: Array<WriteMessageHandler | WriteMessageHandlerBuilder>;
+}
---
+++ @types/contracts/write-message-handler.d.ts
@@ -0,0 +1,7 @@
+import { MessageHandlerBase, LogLevelData } from "simplr-logger";
+export interface WriteMessageHandler extends LogLevelData {
+    Handler: MessageHandlerBase;
+}
+export interface WriteMessageHandlerBuilder extends Partial<LogLevelData> {
+    Handler: MessageHandlerBase;
+}
