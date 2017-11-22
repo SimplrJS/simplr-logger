7891b093dd67b9a6dfe2bee469919ab757b1d6d5
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-11-22 19:15:59 +0200

    Added more methods to helpers.

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,13 @@
 import { LogLevel } from "simplr-logger";
 
-export class HelpersBuilder {
-    public GetLogLevelShortString(level: LogLevel): string {
-        switch (level) {
+export namespace Helpers {
+    /**
+     * Return short name of log level.
+     *
+     * @param logLevel Log level value.
+     */
+    export function GetLogLevelShortString(logLevel: LogLevel): string {
+        switch (logLevel) {
             case LogLevel.Critical:
                 return "crit";
             case LogLevel.Error:
@@ -21,9 +26,58 @@ export class HelpersBuilder {
         }
     }
 
-    public GetLogLevelString(level: LogLevel): string {
-        return LogLevel[level].toString();
+    /**
+     * Return full name of log level.
+     *
+     * @param logLevel Log level value.
+     */
+    export function GetLogLevelString(logLevel: LogLevel): string {
+        return LogLevel[logLevel].toString();
+    }
+
+    /**
+     * Resolve and calculate log level details.
+     *
+     * @param logLevels Log level value or list of log level values.
+     */
+    export function ResolveLogLevel(logLevels: LogLevel | LogLevel[]): { value: LogLevel, isBitMask: boolean } {
+        if (typeof logLevels === "number") {
+            return {
+                value: logLevels,
+                isBitMask: false
+            };
+        } else {
+            return {
+                value: CalculateLogLevelsBitMaskValue(logLevels),
+                isBitMask: true
+            };
+        }
+    }
+
+    /**
+     * Calculate log level bit mask value.
+     *
+     * @param logLevels List of log levels.
+     */
+    export function CalculateLogLevelsBitMaskValue(logLevels: LogLevel[]): LogLevel {
+        let logLevel = LogLevel.None;
+        for (const level of logLevels) {
+            logLevel |= level;
+        }
+
+        return logLevel;
+    }
+
+    /**
+     * Check if log level is enabled in handler.
+     *
+     * @param currentLogLevel Handler current log level.
+     * @param currentLogLevelIsBitMask Handler current log level bit mask value.
+     * @param targetLogLevel Checking log level value.
+     */
+    export function IsLogLevelEnabled(currentLogLevel: LogLevel, currentLogLevelIsBitMask: boolean, targetLogLevel: LogLevel): boolean {
+        return currentLogLevelIsBitMask ?
+            ((currentLogLevel & targetLogLevel) === targetLogLevel) :
+            (currentLogLevel >= targetLogLevel);
     }
 }
-
-export const Helpers = new HelpersBuilder();
