ebfa59f18d7da2365d017a97c9195b57e401f346
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-11-23 11:38:12 +0200

    Reexported prefix type.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Helpers as LoggerHelpers } from "./utils/helpers";
 export * from "./contracts";
 export { LogLevel } from "./abstractions/log-level";
 export { MessageHandlerBase } from "./abstractions/message-handler-base";
+export { PrefixType } from "./abstractions/prefix-type";
 
 export { ConsoleMessageHandler } from "./handlers/console-message-handler";
 export { LoggerConfigurationBuilder } from "./builders/logger-configuration-builder";
